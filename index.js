7a58cf5b7a4d218bec7a33fc636acf80e5aa4b35
Sharon Rossely CAlisson hullunquía Rosas <schullunquia@unsa.edu.pe>
2021-01-26 21:22:24 -0500

    Modifying Routes Definitions

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || "8000";
 */
 
 app.get("/", (req,res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
+    res.status(200).send("WHATABYTE: Food For Devs :)");
 });
 
 /**
