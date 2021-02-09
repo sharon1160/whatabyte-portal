ee2b458b4ecc5a1693ee0e9f9449e143353794f1
Sharon Rossely CAlisson hullunquía Rosas <schullunquia@unsa.edu.pe>
2021-02-09 17:08:53 -0500

    Create a controller for userProfile variable.

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.get("/", (req, res) => {
     res.render("index", {title: "Home"});
 });
 
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
+
 /**
 * Server Activation
 */
